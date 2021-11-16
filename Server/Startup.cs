7bc1f43a6583659742e04bfab9437cc21ca8965b
Zoli <zoli.v.97@gmail.com>
2021-11-17 00:41:20 +0100

    Fixed the internal server issue

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -28,6 +28,10 @@ namespace BlazorPoll.Server
             services.AddRazorPages();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews()
+            .AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
